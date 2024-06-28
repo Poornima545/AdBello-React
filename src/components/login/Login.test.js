733f54687ed028fec20101c71ba4d69cd928a9e6
Poornima545 <poornimac714@gmail.com>
2024-06-28 19:53:00 +0530

    Resolve conflics

--- src/components/login/Login.test.js
+++ src/components/login/Login.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
 import Login from "./Login";
 import { act } from 'react'
 import { MemoryRouter } from "react-router-dom";
